8fcb0bbe2cbcad2516f8f9147d2214bbc8c7a6ee
David Abolade <69032300+A-believer@users.noreply.github.com>
2023-08-29 20:35:12 +0100

    five bugs solved

---
+++ src/utils/requests.ts
@@ -0,0 +1,65 @@
+import {
+  PaginatedRequestParams,
+  PaginatedResponse,
+  RequestByEmployeeParams,
+  SetTransactionApprovalParams,
+  Transaction,
+  Employee,
+} from "./types"
+import mockData from "../mock-data.json"
+
+const TRANSACTIONS_PER_PAGE = 5
+
+const data: { employees: Employee[]; transactions: Transaction[] } = {
+  employees: mockData.employees,
+  transactions: mockData.transactions,
+}
+
+export const getEmployees = (): Employee[] => data.employees
+
+export const getTransactionsPaginated = ({
+  page,
+}: PaginatedRequestParams): PaginatedResponse<Transaction[]> => {
+  if (page === null) {
+    throw new Error("Page cannot be null")
+  }
+
+
+  // Bug 4 
+  const pages =  page * TRANSACTIONS_PER_PAGE
+
+  const start = page
+  const end = pages + TRANSACTIONS_PER_PAGE
+
+  if (start > data.transactions.length) {
+    throw new Error(`Invalid page ${page}`)
+  }
+
+  const nextPage = end < data.transactions.length ? page + 1 : page
+
+  return {
+    nextPage,
+    data: data.transactions.slice(start, end),
+  }
+}
+
+export const getTransactionsByEmployee = ({ employeeId }: RequestByEmployeeParams) => {
+  if (!employeeId) {
+    // Bug 3 
+    return data.transactions
+  }
+
+  return data.transactions.filter((transaction) => transaction.employee.id === employeeId)
+}
+
+export const setTransactionApproval = ({ transactionId, value }: SetTransactionApprovalParams): void => {
+  const transaction = data.transactions.find(
+    (currentTransaction) => currentTransaction.id === transactionId
+  )
+
+  if (!transaction) {
+    throw new Error("Invalid transaction to approve")
+  }
+
+  transaction.approved = value
+}
